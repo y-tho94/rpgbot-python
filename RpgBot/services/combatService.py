9b7bdea3245d01bc3300b3c389f084eeb4bc2538
Jacob Frey <jakefreydo@gmail.com>
2026-07-08 13:18:02 -0400

    fix for buff math

--- RpgBot/services/combatService.py
+++ RpgBot/services/combatService.py
@@ -94,8 +94,9 @@ class CombatService():
         selfHealAmount = 0 if ability.Effects.SelfHeal == 0 else ability.Effects.SelfHeal + modifier
         inflictAmount = 0 if ability.Effects.Inflict == 0 else ability.Effects.Inflict + modifier
         selfInflictAmount = ability.Effects.SelfInflict
-        boostAmount = 0 if len(ability.Effects.Boost) == 0 else math.floor(math.log10(modifier) * 5)
-        debuffAmount = 0 if len(ability.Effects.Debuff) == 0 else math.floor(math.log10(modifier) * 5)
+        modifier = max(1, modifier)
+        boostAmount = 0 if len(ability.Effects.Boost) == 0 or modifier <= 0  else math.floor(math.log10(modifier) * 5)
+        debuffAmount = 0 if len(ability.Effects.Debuff) == 0 or modifier <= 0 else math.floor(math.log10(modifier) * 5)
 
         #boost stats
         for stat in ability.Effects.Boost:
